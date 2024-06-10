58e925eff7ca511b6cc72c2fcd06cbec8516612e
unknown <emarkle99@gmail.com>
2024-06-10 15:45:09 -0500

    quit button quits

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// Main menu methods
 public class MainMenu : MonoBehaviour
-{
+{  
+    // starts maze scene
     public void PlayMaze()
     {
         SceneManager.LoadScene("maze");
+    }
+
+    // quits application
+    public void QuitMaze()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
     }
 }
