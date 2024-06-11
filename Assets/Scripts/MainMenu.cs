35c9e590dca6d758d308548389889fbe3e3e8d0d
unknown <emarkle99@gmail.com>
2024-06-11 17:34:42 -0500

    colorblind toggler!

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,39 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 /// Main menu methods
 public class MainMenu : MonoBehaviour
 {  
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+    private Color normalTrapColor;
+    private Color normalGoalColor;
+
+
+    void Start()
+    {
+        normalTrapColor = trapMat.color;
+        normalGoalColor = goalMat.color;
+
+        colorblindMode.isOn = false;
+    }
+
     // starts maze scene
     public void PlayMaze()
     {
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        else
+        {
+            trapMat.color = normalTrapColor;
+            goalMat.color = normalGoalColor;
+        }
+
         SceneManager.LoadScene("maze");
     }
 
